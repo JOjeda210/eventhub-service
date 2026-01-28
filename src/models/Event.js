527da2a9b264c29fab7bdd64af589b698cd299ff
Jesús Ojeda <ojedajesus232@gmail.com>
2026-01-27 21:21:42 -0500

    feat: add Event model

---
+++ src/models/Event.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+    title : {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    description: {
+        type: String,
+        required: true
+    },
+    date : {
+        type: Date,
+        required: true
+    },
+    location : {
+        address: {
+            type: String,
+        },
+        lat : {
+            type:Number
+        },
+        lng :{
+            type:Number
+        }
+    },
+    price : {
+        type:Number,
+        default : 0
+    },
+    isPaid :{
+        type: Boolean,
+        default : false
+    },
+    eventType:{
+        type: String
+    },
+    weather:{
+        type: Object
+    },
+    spotify:{
+        type: Object
+    },
+    // creator: {
+
+    // }
+
+},
+    {timestamps: true}
+);
+
+const Event = mongoose.model('Event',eventSchema);
+export default Event;
