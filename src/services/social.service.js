1998240b888094132d8cdd710dffd7f1d03e0eb9
Jesús Ojeda <ojedajesus232@gmail.com>
2026-02-24 14:49:38 -0500

    feat: add inital logic for the social service

---
+++ src/services/social.service.js
@@ -0,0 +1,21 @@
+
+export const socialService = {
+    generateShareUrl: (platform, eventData) => {
+        if(!eventData || eventData.eventUrl){
+            throw new Error('event data and eventUrl are required'); 
+        }
+
+        switch (platform) {
+            case 'facebook':
+
+                return `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(eventData.eventUrl)}`
+
+            case 'twitter':
+                return `https://twitter.com/intent/tweet?text=${encodeURIComponent(eventData.title)}&url=${encodeURIComponent(eventData.eventUrl)}`
+
+            default:
+                throw new Error("Platform not supported");
+        }
+
+    }
+}
